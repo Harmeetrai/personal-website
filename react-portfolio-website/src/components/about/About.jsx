e91aa3345e14136f5f21d5eb17eb4616b9e7d2c2
Harmeetrai <harmeetrai18@gmail.com>
2022-05-12 15:35:18 -0700

    update components

--- react-portfolio-website/src/components/about/About.jsx
+++ react-portfolio-website/src/components/about/About.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assests/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+ 
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='me' />
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className="about__cards">
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years in Experience</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon' />
+              <h5>University</h5>
+              <small>Data Science student at Simon Fraser University</small>
+            </article>
+
+            <article className='about__card'>
+              <FaAward className='about__icon' />
+              <h5>Experience</h5>
+              <small>3+ Years in Experience</small>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
